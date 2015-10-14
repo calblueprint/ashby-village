d55492b56503c2146d025f64528484cd6c8fd677
Daniel Li <danielmxli@gmail.com>
2015-10-14 01:16:19 -0700

    Add name, DOB, number to user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Default URL options for the Devise mailer
+  # Default URL options for the Devise action_mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014074255) do
+ActiveRecord::Schema.define(version: 20151014080626) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20151014074255) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.date     "date_of_birth"
+    t.string   "phone",                  limit: 255
+    t.string   "cell_phone",             limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
