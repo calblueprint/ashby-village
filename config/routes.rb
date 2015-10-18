94b60925cb01733b022c58350fd9b1a4a67d0d79
shannonchu <shannonchu@berkeley.edu>
2015-10-17 19:00:04 -0700

    Create home, help, and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  root 'static_pages#home'
+  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'}
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
