5da5638e921540b1782d36302aed55e1fafbef68
Helen Che <helenc@berkeley.edu>
2015-10-19 21:53:32 -0700

    Remove user col from nh and vice versa

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020033930) do
+ActiveRecord::Schema.define(version: 20151020045234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20151020033930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "location"
-    t.string   "user"
     t.integer  "group_id"
   end
 
@@ -74,7 +73,6 @@ ActiveRecord::Schema.define(version: 20151020033930) do
     t.date     "date_of_birth"
     t.string   "phone"
     t.string   "cell_phone"
-    t.string   "neighborhood"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
