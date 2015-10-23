9b203459e0c6571644af14f88a01db2bcda21426
shannonchu <shannonchu@berkeley.edu>
2015-10-22 21:14:12 -0700

    Revert migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021031314) do
+ActiveRecord::Schema.define(version: 20151023040340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,13 +75,15 @@ ActiveRecord::Schema.define(version: 20151021031314) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "neighborhood"
-    t.string   "group"
+    t.date     "date_of_birth"
     t.string   "phone"
+    t.string   "cell_phone"
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "neighborhood"
+    t.string   "group"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
