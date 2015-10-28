958412faf5458184c92d3cb32cd3021bca15594f
Nikita Rau <raun@berkeley.edu>
2015-10-27 21:50:40 -0700

    add back groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   def index
-    @group = Group.all
+    @groups = Group.all
   end
 
   def show
@@ -9,27 +9,22 @@ class GroupsController < ApplicationController
   end
 
   def new
-    ####### @neighborhood = Neighborhood.find params[:neighborhood_id]
-    @group = @neighborhood.groups.build
+    @group = Group.new
   end
 
   def create
-    ####### @neighborhood = Neighborhood.find params[:neighborhood_id]
-    @group = @neighborhood.groups.build group_params
-    if @group.save
-      ####### redirect_to neighborhood_group_path @neighborhood, @group, notice: "Group was successfully created."
-    else
-      render :new
+    @group = Group.new(group_params)
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: "Group was successfully created." }
+      else
+        format.html { render :new }
+      end
     end
-  end
-
-  def member_listing
-  	@group = Group.find(params[:id])
-  	@members = @group.users
   end
 
   def group_params
     params.require(:group).permit(:name, :description)
   end
-
 end
+
