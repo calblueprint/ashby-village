251498a6e0fa99f3866c319833de84a7254eae34
Helen Che <helenc@berkeley.edu>
2015-11-02 16:04:57 -0800

    Render groups on dropdown using .show()/.hide() methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,20 +3,6 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
     @neighborhoods = Neighborhood.all
-    # render json: @groups
-
-    if params[:ide] == nil || params[:ide] === "all"
-      groups = @groups
-    else
-      id = params[:ide].to_i
-      neighborhood = Neighborhood.find(id)
-      groups = neighborhood.groups
-    end
-
-    respond_to do |format|
-      format.html  # index.html.erb
-      format.json  { render :json => groups }
-    end
   end
 
   def show
