406a136e73df84d3609e83a66ad3fb930618bf98
shannonchu <shannonchu@berkeley.edu>
2015-11-05 13:02:03 -0800

    Passwords controller

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,22 @@
 class Users::PasswordsController < Devise::PasswordsController
+  skip_before_filter :require_no_authentication, :only => [:edit, :update]
+
+  def new
+    super
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(params[resource_name])
+    if resource.errors.empty?
+      sign_out(resource_name)
+      sign_in(resource_name, resource)
+    end
+  end
+
   # GET /resource/password/new
   # def new
   #   super
