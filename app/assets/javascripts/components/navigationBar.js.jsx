0f3611c296c0036d6f382879dff942c9104417fb
Andy Qin <andrewlqin@gmail.com>
2015-11-06 17:32:08 -0800

    Add missing files

---
+++ app/assets/javascripts/components/navigationBar.js.jsx
@@ -0,0 +1,9 @@
+var NavigationBar = React.createClass({
+  render: function() {
+    return (
+      <nav className="row">
+        <img src={"http://s3.amazonaws.com/ClubExpressClubFiles/748044/graphics/AVlogocolor-web.png"} />
+      </nav>
+    );
+  }
+});
