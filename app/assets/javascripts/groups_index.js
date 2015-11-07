07d4b6ff5a1c20bd042a29d9744a983a0480d84f
Helen Che <helenc@berkeley.edu>
2015-11-07 12:00:00 -0800

    Move js indo seperate file

--- app/assets/javascripts/groups_index.js
+++ app/assets/javascripts/groups_index.js
@@ -1,1 +1,26 @@
-var helloWorld = 'Hello world!';
+$(document).ready(function(){
+  var selectKind = $('.selectKind').val();
+  var selectNeighborhood = $('.selectNeighborhoods').val();
+
+  function groupList(selKind, selNeighborhood) {
+    $('tr').show();
+    if (selKind != 'all' && selNeighborhood != 'all') {
+      $('tr').not('.' + selNeighborhood).hide();
+      $('tr').not('.' + selKind).hide();
+    } else if (selectKind == 'all' && selNeighborhood != 'all') {
+      $('tr').not('.' + selNeighborhood).hide();
+    } else if (selNeighborhood == 'all' && selKind != 'all') {
+      $('tr').not('.' + selectKind).hide();
+    }
+  }
+
+  $('.selectKind').change(function(){
+    selectKind = $(this).val();
+    groupList(selectKind, selectNeighborhood);
+  });
+
+  $('.selectNeighborhoods').change(function(){
+    selectNeighborhood = $(this).val();
+    groupList(selectKind, selectNeighborhood);
+  });
+});
