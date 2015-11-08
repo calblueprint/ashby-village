7189362b1f20c52bd44dd446c2057c7002d7ac9a
Andy Qin <andrewlqin@gmail.com>
2015-11-07 17:24:45 -0800

    Get rid of unused navbar react component

--- app/assets/javascripts/components/navigationBar.js.jsx
+++
@@ -1,9 +0,0 @@
-var NavigationBar = React.createClass({
-  render: function() {
-    return (
-      <nav className="row">
-        <img src={"http://s3.amazonaws.com/ClubExpressClubFiles/748044/graphics/AVlogocolor-web.png"} />
-      </nav>
-    );
-  }
-});
