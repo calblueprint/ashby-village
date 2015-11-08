8487faeccfcc71cd71fb5d5e730b570e0afde8ca
shannonchu <shannonchu@berkeley.edu>
2015-11-07 21:19:07 -0800

    Merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,12 @@ Rails.application.routes.draw do
   end
   root 'static_pages#home'
 
-<<<<<<< HEAD
-  devise_for :users, :path => '',
-  :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'},
-  :controllers => { :registrations => 'users/registrations' }
-
-  resources :users, :only => [:show, :edit, :update]
-=======
-
   ##################################################
   # Devise
   ##################################################
-  devise_for :users, :path => '', :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => 'users/registrations' }
+  devise_for :users, :path => '',
+  :path_names => {:sign_up => 'register', :sign_in => 'login', :sign_out => 'logout'},
+  :controllers => { :registrations => 'users/registrations' }
 
   ##################################################
   # Sessions
@@ -28,17 +22,12 @@ Rails.application.routes.draw do
   ##################################################
   # Users
   ##################################################
-  get 'user/show'
-
-  get 'user/:id', to: "user#show", as: 'user'
->>>>>>> master
+  resources :users, :only => [:show, :edit, :update]
 
   ##################################################
   # Groups
   ##################################################
   resources :groups
   get "groups/:id/member_listing", to: 'groups#member_listing', as:'member_listing'
-
-  # get 'groups/:id', to: 'users#show'
 
 end
