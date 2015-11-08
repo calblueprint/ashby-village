b55d05f1d9708a31b829e0d96da17e22197bc48b
Andy Qin <andrewlqin@gmail.com>
2015-11-07 21:34:04 -0800

    Add member count into each group tile

--- app/assets/javascripts/components/_group_tile.js.jsx
+++ app/assets/javascripts/components/_group_tile.js.jsx
@@ -1,4 +1,11 @@
 // The React file defining each group tile.
+var MemberSection = React.createClass({
+	render: function() {
+		return (
+			<h6>{this.props.member_count} MEMBERS</h6>
+		);
+	}
+});
 
 var GroupTile = React.createClass({
 	render: function() {
@@ -6,7 +13,8 @@ var GroupTile = React.createClass({
 			<div className="group-tile row">
 				<h3>{this.props.name}</h3>
 				<h6>{this.props.kind.toUpperCase()} GROUP IN {this.props.neighborhood.toUpperCase()}</h6>
+				<MemberSection member_count={this.props.member_count} />
 			</div>
-		)
+		);
 	}
 });
