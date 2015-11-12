f02258ed63311a9aecde3b25a67e6b99c6707cd3
Andy Qin <andrewlqin@gmail.com>
2015-11-11 21:02:11 -0800

    Update seed data file to have group photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}"))
+end
 
 def make_groups
-  Group.create! name: 'Movies', description: 'yo', neighborhood_id: 1, kind: 1
-  Group.create! name: 'Knitting', description: 'yo', neighborhood_id: 1, kind: 0
-  Group.create! name: 'Computer', description: 'yo', neighborhood_id: 1, kind: 2
-  Group.create! name: 'Poker', description: 'yo', neighborhood_id: 1, kind: 0
-  Group.create! name: 'Bingo', description: 'yo', neighborhood_id: 1, kind: 1
-  Group.create! name: 'Music', description: 'yo', neighborhood_id: 1, kind: 2
-  Group.create! name: 'Reading', description: 'yo', neighborhood_id: 1, kind: 0
-  Group.create! name: 'Politics', description: 'yo', neighborhood_id: 2, kind: 1
-  Group.create! name: 'Education', description: 'yo', neighborhood_id: 2, kind: 2
-  Group.create! name: 'Kites', description: 'yo', neighborhood_id: 2, kind: 0
-  Group.create! name: 'Pool', description: 'yo', neighborhood_id: 2, kind: 1
-  Group.create! name: 'Checkers', description: 'yo', neighborhood_id: 2, kind: 2
-  Group.create! name: 'Chess', description: 'yo', neighborhood_id: 3, kind: 0
-  Group.create! name: 'Frisbee', description: 'yo', neighborhood_id: 3, kind: 1
-  Group.create! name: 'Travel', description: 'yo', neighborhood_id: 3, kind: 2
-  Group.create! name: 'Television', description: 'yo', neighborhood_id: 3, kind: 0
-  Group.create! name: 'Ashby Village Announcements', description: 'yo', neighborhood_id: 2, kind: 1
+  Group.create! name: 'Movies', description: 'yo', neighborhood_id: 1, kind: 1, photo: seed_image("jazzercise.png")
+  Group.create! name: 'Knitting', description: 'yo', neighborhood_id: 1, kind: 0, photo: seed_image("gaming.png")
+  Group.create! name: 'Computer', description: 'yo', neighborhood_id: 1, kind: 2, photo: seed_image("film_fanatics.png")
+  Group.create! name: 'Poker', description: 'yo', neighborhood_id: 1, kind: 0, photo: seed_image("skydiving.png")
+  Group.create! name: 'Bingo', description: 'yo', neighborhood_id: 1, kind: 1, photo: seed_image("jazzercise.png")
+  Group.create! name: 'Music', description: 'yo', neighborhood_id: 1, kind: 2, photo: seed_image("gaming.png")
+  Group.create! name: 'Reading', description: 'yo', neighborhood_id: 1, kind: 0, photo: seed_image("film_fanatics.png")
+  Group.create! name: 'Politics', description: 'yo', neighborhood_id: 2, kind: 1, photo: seed_image("skydiving.png")
+  Group.create! name: 'Education', description: 'yo', neighborhood_id: 2, kind: 2, photo: seed_image("jazzercise.png")
+  Group.create! name: 'Kites', description: 'yo', neighborhood_id: 2, kind: 0, photo: seed_image("gaming.png")
+  Group.create! name: 'Pool', description: 'yo', neighborhood_id: 2, kind: 1, photo: seed_image("film_fanatics.png")
+  Group.create! name: 'Checkers', description: 'yo', neighborhood_id: 2, kind: 2, photo: seed_image("skydiving.png")
+  Group.create! name: 'Chess', description: 'yo', neighborhood_id: 3, kind: 0, photo: seed_image("jazzercise.png")
+  Group.create! name: 'Frisbee', description: 'yo', neighborhood_id: 3, kind: 1, photo: seed_image("gaming.png")
+  Group.create! name: 'Travel', description: 'yo', neighborhood_id: 3, kind: 2, photo: seed_image("film_fanatics.png")
+  Group.create! name: 'Television', description: 'yo', neighborhood_id: 3, kind: 0, photo: seed_image("skydiving.png")
+  Group.create! name: 'Ashby Village Announcements', description: 'yo', neighborhood_id: 2, kind: 1, photo: seed_image("jazzercise.png")
 end
 
 def make_users
-  User.create! first_name: 'Laila', last_name: 'Rowe', email: 'blah@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Mark', last_name: 'Rowe', email: 'blah1@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Jenny', last_name: 'Rowe', email: 'blah2@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Sandra', last_name: 'Rowe', email: 'blah3@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'John', last_name: 'Rowe', email: 'blah4@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Eric', last_name: 'Rowe', email: 'blah5@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Dennis', last_name: 'Rowe', email: 'blah6@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
-  User.create! first_name: 'Ajay', last_name: 'Rowe', email: 'blah7@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok'
+  User.create! first_name: 'Laila', last_name: 'Rowe', email: 'blah@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("nikita.jpg")
+  User.create! first_name: 'Mark', last_name: 'Rowe', email: 'blah1@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("andy.jpg")
+  User.create! first_name: 'Jenny', last_name: 'Rowe', email: 'blah2@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("shannon.jpg")
+  User.create! first_name: 'Sandra', last_name: 'Rowe', email: 'blah3@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("helen.jpg")
+  User.create! first_name: 'John', last_name: 'Rowe', email: 'blah4@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("andy.jpg")
+  User.create! first_name: 'Eric', last_name: 'Rowe', email: 'blah5@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("daniel.jpg")
+  User.create! first_name: 'Dennis', last_name: 'Rowe', email: 'blah6@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("andy.jpg")
+  User.create! first_name: 'Ajay', last_name: 'Rowe', email: 'blah7@gmail.com', phone: '917', cell_phone: '917', date_of_birth: 'date', password: 'okokokokok', photo: seed_image("daniel.jpg")
 end
 
 def make_user_groups
