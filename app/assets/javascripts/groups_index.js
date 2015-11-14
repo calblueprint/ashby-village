7f3951639b4df0afa06c6bf579307d21e9a79c1b
Andy Qin <andrewlqin@gmail.com>
2015-11-13 18:58:30 -0800

    Add jQuery logic to make groups appear properly

--- app/assets/javascripts/groups_index.js
+++ app/assets/javascripts/groups_index.js
@@ -1,26 +1,50 @@
-$(document).ready(function(){
-  var selectKind = $('.selectKind').val();
-  var selectNeighborhoods = $('.selectNeighborhoods').val();
+$(function() {
+  // var kind = $(".group-dropdown").val();
+  // var neighborhood = $(".neighborhood-dropdown").val();
 
-  function groupList(selKind, selNeighborhood) {
-    $('tr').show();
-    if (selKind != 'all' && selNeighborhood != 'all') {
-      $('tr').not('.' + selNeighborhood).hide();
-      $('tr').not('.' + selKind).hide();
-    } else if (selKind == 'all' && selNeighborhood != 'all') {
-      $('tr').not('.' + selNeighborhood).hide();
-    } else if (selKind != 'all' && selNeighborhood == 'all') {
-      $('tr').not('.' + selKind).hide();
+  $(".group-dropdown, .neighborhood-dropdown").change(function() {
+    var kind = $(".group-dropdown").val();
+    var neighborhood = $(".neighborhood-dropdown").val();
+
+    $(".group-tile").show();
+    if (kind != "all") {
+      $(".group-tile").not("." + kind).hide();
     }
-  }
+    if (neighborhood != "all") {
+      $(".group-tile").not("." + neighborhood).hide();
+    }
 
-  $('.selectKind').change(function(){
-    selectKind = $(this).val();
-    groupList(selectKind, selectNeighborhoods);
   });
 
-  $('.selectNeighborhoods').change(function(){
-    selectNeighborhoods = $(this).val();
-    groupList(selectKind, selectNeighborhoods);
-  });
 });
+
+
+// $(document).ready(function(){
+//   var selectKind = $('.selectKind').val();
+//   var selectNeighborhoods = $('.selectNeighborhoods').val();
+
+//   function groupList(selKind, selNeighborhood) {
+//     $('tr').show();
+//     if (selKind != 'all' && selNeighborhood != 'all') {
+//       $('tr').not('.' + selNeighborhood).hide();
+//       $('tr').not('.' + selKind).hide();
+//     } else if (selKind == 'all' && selNeighborhood != 'all') {
+//       $('tr').not('.' + selNeighborhood).hide();
+//     } else if (selKind != 'all' && selNeighborhood == 'all') {
+//       $('tr').not('.' + selKind).hide();
+//     }
+//   }
+
+//   $('.selectKind').change(function(){
+//     selectKind = $(this).val();
+//     groupList(selectKind, selectNeighborhoods);
+//   });
+
+//   $('.selectNeighborhoods').change(function(){
+//     selectNeighborhoods = $(this).val();
+//     groupList(selectKind, selectNeighborhoods);
+//   });
+// });
+
+
+
