17208fb68ba4e576676cc09027ac69635eff73e6
Daniel Li <danielmxli@gmail.com>
2015-11-15 12:43:09 -0800

    Merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114221809) do
+ActiveRecord::Schema.define(version: 20151115022900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20151114221809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "group_id"
+    t.text     "title"
   end
 
   add_index "posts", ["group_id"], name: "index_posts_on_group_id", using: :btree
