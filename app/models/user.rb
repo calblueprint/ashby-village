e94ae928399f3849de8fa6e28a2bed0c9c008231
Nikita Rau <raun@berkeley.edu>
2015-11-17 13:39:57 -0800

    Add searchkick gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  searchkick
 
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
 
