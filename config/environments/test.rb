20013c22dae5b096a0180ec393b22d8db88e225b
Daniel Li <danielmxli@gmail.com>
2015-11-17 18:18:06 -0800

    Change url to something insane, but works

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    url: 'ashby-staging.s3.amazonaws.com',
+    url: ':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
