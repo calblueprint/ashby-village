1b9bfc47da146bd21bfdca9663a458b8cd017e25
Daniel Li <danielmxli@gmail.com>
2015-11-21 00:41:34 -0800

    Add condition for remove user

--- app/models/group.rb
+++ app/models/group.rb
@@ -18,7 +18,7 @@
 
 class Group < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :name, :use => :slugged
+  friendly_id :name, use: :slugged
 
   enum kind: [:social, :neighborhood, :committee]
   enum state: [:inactive, :active]
@@ -30,6 +30,8 @@ class Group < ActiveRecord::Base
     def leaders
       where("user_groups.is_leader = ?", true)
     end
+    # def has_many_leaders
+    #   where()
   end
 
   has_many :posts, dependent: :destroy
@@ -37,15 +39,21 @@ class Group < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "150x150#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
-  def add_user(current_user, make_leader=false)
+  def add_user(current_user, make_leader = false)
     UserGroup.create(user: current_user, group: self, is_leader: make_leader)
   end
 # Case 1: They are a normal member and they want to remove themselves
+# => remove user
 # Case 2: They are a leader with NO other leaders
-# => make group "dead"
+# => make group inactive, remove user
 # Case 3: They are a leader with other leaders
-# => They remove themselves
-  def remove_user(current_user)
-    UserGroup.find_
+# => remove user
 
+  def remove_user(current_user)
+    # If the current user is the only leader, make the group inactive
+    if @group.users.leaders.count == 1 && @group.users.leaders.exists?(id: current_user.id)
+      @group.update_attribute :state, 0
+    end
+    @group.users.delete(current_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115022900) do
+ActiveRecord::Schema.define(version: 20151121075523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151115022900) do
     t.datetime "photo_updated_at"
     t.string   "slug"
     t.integer  "kind"
+    t.integer  "state"
   end
 
   add_index "groups", ["neighborhood_id"], name: "index_groups_on_neighborhood_id", using: :btree
