23be93e9f133ba12c4317fa771246d6f3ea47c1a
Daniel Li <danielmxli@gmail.com>
2015-11-21 15:57:40 -0800

    Add auth in rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,11 +2,15 @@ RailsAdmin.config do |config|
 
   ### Popular gems integration
 
-  # == Devise ==
-  config.authenticate_with do
-    warden.authenticate! scope: :user
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+  # TODO (shimmy): Use cancancan for this!
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.try(:admin?)
   end
-  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
