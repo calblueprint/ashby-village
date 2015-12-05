0a6ca739d841ca1e44154f8280e9781f0405010a
Andy Qin <andrewlqin@gmail.com>
2015-12-04 16:52:19 -0800

    Comment out accessibility until the feature is more polished

--- app/assets/javascripts/accessibility.js
+++ app/assets/javascripts/accessibility.js
@@ -1,22 +1,23 @@
 /*
 	Ashby Village's custom JavaScript library for accessibility features.
+	Currently a WIP.
 */
 
-var ready = function() {
+// var ready = function() {
 
-	// Highlight text to read aloud feature
-	var t = '';
-	function gText(e) {
-		t = (document.all) ? document.selection.createRange().text : document.getSelection();
-		var utterance = new SpeechSynthesisUtterance(t);
-		var voices = window.speechSynthesis.getVoices();
-		utterance.voice = voices[1];
-		window.speechSynthesis.speak(utterance);
-	}
-	document.onmouseup = gText;
-	if (!document.all) document.captureEvents(Event.MOUSEUP);
+// 	// Highlight text to read aloud feature
+// 	var t = '';
+// 	function gText(e) {
+// 		t = (document.all) ? document.selection.createRange().text : document.getSelection();
+// 		var utterance = new SpeechSynthesisUtterance(t);
+// 		var voices = window.speechSynthesis.getVoices();
+// 		utterance.voice = voices[1];
+// 		window.speechSynthesis.speak(utterance);
+// 	}
+// 	document.onmouseup = gText;
+// 	if (!document.all) document.captureEvents(Event.MOUSEUP);
 
-}
+// }
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
