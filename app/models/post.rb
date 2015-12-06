4c2038a5f54c425d6d33dc1e8270921c91c94ffe
Daniel Li <danielmxli@gmail.com>
2015-12-05 21:24:19 -0800

    Add validations on the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  # validates :user_id, presence: true
-  # validates :content, presence: true
+  validates :user_id, presence: true
+  validates :content, presence: true
 
   # This will allow order_by_created_at to work, as well as order_by_created_at(:asc)
   scope :order_by_created_at, -> (direction = :desc) { order(created_at: direction) }
