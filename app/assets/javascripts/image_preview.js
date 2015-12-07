4f5ccbcbf3a93b76237450f78704cc925d292c4e
shannonchu <shannonchu@berkeley.edu>
2015-12-07 12:06:40 -0800

    Save image to user

---
+++ app/assets/javascripts/image_preview.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(".uploadFile").on("change", function()
+    {
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+
+        if (/^image/.test( files[0].type)){ // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+
+            reader.onloadend = function(){ // set image data as background of div
+                $(".imagePreview").css("background-image", "url("+this.result+")");
+            }
+        }
+    });
+});
