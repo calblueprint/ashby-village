34f8634887d4d72df3cad32826165ca9b3c019e0
Daniel Li <danielmxli@gmail.com>
2015-12-06 20:29:08 -0800

    Add delete style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    # @post = Post.find(params[:id])
     @post.destroy
     flash[:info] = "Post deleted"
     redirect_to :back
@@ -27,7 +26,7 @@ class PostsController < ApplicationController
 
   # TODO (Shimmy): Use CanCanCan instead.
   def correct_user
-    @post = current_user.post.find_by(id: params[:id])
+    @post = current_user.posts.find_by(id: params[:id])
     redirect_to root_url if @post.nil?
   end
 end
