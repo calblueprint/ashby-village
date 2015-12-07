8db95aed343b4261b671e882edf04d2a820ba084
shannonchu <shannonchu@berkeley.edu>
2015-12-07 14:12:25 -0800

    almost working just need to modify source

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -1,19 +1,20 @@
-var ready =
-  $(function() {
-    {
-      var files = !!this.files ? this.files : [];
-      if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+var ready = function() {
+  $(".uploadFile").on("change", function()
+  {
+    var files = !!this.files ? this.files : [];
+    if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
 
-      if (/^image/.test( files[0].type)){ // only image file
-        var reader = new FileReader(); // instance of the FileReader
-        reader.readAsDataURL(files[0]); // read the local file
+    if (/^image/.test( files[0].type)){ // only image file
+      var reader = new FileReader(); // instance of the FileReader
+      reader.readAsDataURL(files[0]); // read the local file
 
-        reader.onloadend = function(){ // set image data as background of div
-          $(".imagePreview").css("background-image", "url("+this.result+")");
-        }
+      reader.onloadend = function(){ // set image data as background of div
+        $(".photoPreview").css("background-image", "url("+this.result+")");
+        $(".photoPreview").attr("src", "+this.result+");
       }
-    });
+    }
   });
+};
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
