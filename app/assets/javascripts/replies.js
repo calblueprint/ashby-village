96f3db76dcc75f0eefdd93533dd3f0325b133db9
Helen Che <helenc@berkeley.edu>
2015-12-08 09:51:06 -0800

    Test for bug

--- app/assets/javascripts/replies.js
+++ app/assets/javascripts/replies.js
@@ -1,25 +1,25 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-var ready = function() {
-  $(".reply-button").hide();
+// // Place all the behaviors and hooks related to the matching controller here.
+// // All this logic will automatically be available in application.js.
+// var ready = function() {
+//   $(".reply-button").hide();
 
-  $( ".reply-content" ).focus(function() {
-    // TODO: Use next function instead?
-    $( this ).siblings(".reply-button").show();
+//   $( ".reply-content" ).focus(function() {
+//     // TODO: Use next function instead?
+//     $( this ).siblings(".reply-button").show();
+//     // console.log($( this ).siblings(".reply-button"));
+//     var container = $(this);
+//     hideReplyContainer(container);
+//   });
 
-    var container = $(this);
-    hideReplyContainer(container);
-  });
-
-  function hideReplyContainer(container) {
-    $(document).mouseup(function (e) {
-      var input = container.val().length;
-      console.log(container.val().length);
-      if (!container.is(e.target) && container.has(e.target).length === 0 && input === 0) {
-          container.siblings(".reply-button").hide();
-      }
-    });
-  }
-}
-$(document).ready(ready);
-$(document).on("page:load", ready);
+//   function hideReplyContainer(container) {
+//     $(document).mouseup(function (e) {
+//       var input = container.val().length;
+//       console.log(container.val().length);
+//       if (!container.is(e.target) && container.has(e.target).length === 0 && input === 0) {
+//           container.siblings(".reply-button").hide();
+//       }
+//     });
+//   }
+// }
+// $(document).ready(ready);
+// $(document).on("page:load", ready);
