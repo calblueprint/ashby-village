00668423eb6e99669bc14111eb62cc310c9d599a
Daniel Li <danielmxli@gmail.com>
2015-12-07 21:00:43 -0800

    Refactor and fix image preview js

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -1,20 +1,18 @@
-var ready = function() {
-  $(".uploadFile").on("change", function()
-  {
-    var files = !!this.files ? this.files : [];
-    if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
-
-    if (/^image/.test( files[0].type)){ // only image file
-      var reader = new FileReader(); // instance of the FileReader
-      reader.readAsDataURL(files[0]); // read the local file
-
-      reader.onloadend = function(){ // set image data as background of div
-        $(".photoPreview").css("background-image", "url("+this.result+")");
-        $(".photoPreview").attr("src", '');
-      }
+var ready = (function() {
+  $('.uploadFile').on('change', function(event) {
+    var files = event.target.files;
+    var image = files[0]
+    var reader = new FileReader();
+    reader.onload = function(file) {
+      var img = new Image();
+      img.src = file.target.result;
+      $('.photoPreview').attr("src",img.src);
     }
+    reader.readAsDataURL(image);
+    console.log("files",files);
+    console.log("image", image);
   });
-};
+});
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
