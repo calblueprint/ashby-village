2989abda098d95d8395db23bcd7ed193462d1ce4
Daniel Li <danielmxli@gmail.com>
2015-12-08 10:20:21 -0800

    Change sessions controller to custom. Not finished.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,19 +2,26 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    if sign_in(resource_name, resource)
+      flash[:notice] = "haah yee!"
+      redirect_to groups_path
+    else
+      flash[:alert] = "Nah"
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
