bcabbf00551ce2daefd5ae8a560c36b4c962fbb9
Daniel Li <danielmxli@gmail.com>
2015-12-10 01:54:58 -0800

    Stop playing

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,32 +2,32 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
-
-  # POST /resource/sign_in
-  def create
-    byebug
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    byebug
-    if sign_in(resource_name, resource)
-      respond_to do |format|
-        format.js { flash[:notice] = "haah yee!" }
-      end
-      redirect_to groups_path
-    else
-      respond_to do |format|
-        format.js { flash[:notice] = "nahhhh try again doe!" }
-      end
-    end
-  end
-
-  # DELETE /resource/sign_out
-  def destroy
-    super
-  end
+  # def new
+  #   super
+  # end
+  #
+  # # POST /resource/sign_in
+  # def create
+  #   byebug
+  #   self.resource = warden.authenticate!(auth_options)
+  #   set_flash_message(:notice, :signed_in) if is_flashing_format?
+  #   byebug
+  #   if sign_in(resource_name, resource)
+  #     respond_to do |format|
+  #       format.js { flash[:notice] = "haah yee!" }
+  #     end
+  #     redirect_to groups_path
+  #   else
+  #     respond_to do |format|
+  #       format.js { flash[:notice] = "nahhhh try again doe!" }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
   # protected
 
