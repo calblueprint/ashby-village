e8054cedd1a0122863b43883c9057ead135b9714
Helen Che <helenc@berkeley.edu>
2015-12-09 16:08:08 -0800

    Add join group button to group page while having the settings button/functionality appear without page reload

--- app/assets/javascripts/groups_show.js
+++ app/assets/javascripts/groups_show.js
@@ -1,4 +1,17 @@
 var ready = function() {
+  $(".group-join").click(function(){
+    $.ajax({
+      type: "PUT",
+      url: "/groups/" + $(this).attr("value") + "/join"
+    });
+    $(this).removeClass("group-join");
+    $(this).addClass("group-joined");
+    $(this).prop("disabled", true);
+    $(this).text("You Have Joined");
+    $(".false").show();
+  });
+
+  $(".false").hide();
   $(".group-new-post").show();
   $(".group-posts").show();
   $(".click-new-post").addClass("selected");
@@ -7,7 +20,7 @@ var ready = function() {
 
   $(".click-new-post").click(function() {
     $(".click-member-listing").removeClass("selected");
-    $(".click-group-settings").removeClass("selected");
+    $(".click-group-settings-true").removeClass("selected");
     $(this).addClass("selected");
     $(".group-new-post").show();
     $(".group-posts").show();
@@ -17,7 +30,7 @@ var ready = function() {
 
   $(".click-member-listing").click(function() {
     $(".click-new-post").removeClass("selected");
-    $(".click-group-settings").removeClass("selected");
+    $(".click-group-settings-true").removeClass("selected");
     $(this).addClass("selected");
     $(".group-new-post").hide();
     $(".group-member-listing").show();
