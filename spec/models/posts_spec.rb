afd2dd17357dfa9108179bee9de4d9917112cfbd
piccoloman <joeypereira@sbcglobal.net>
2016-01-30 14:17:47 -0800

    Added Group_id to Post test
    Summary: Added group_id to Post test
    
    Test Plan: this is a test
    
    Projects: #ashby
    
    Differential Revision: http://phab.calblueprint.org/D153

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -9,25 +9,30 @@ u1 = User.create! first_name: "Rosie",\
                   date_of_birth: "date",\
                   password: "okokokokok",\
                   role: 0
+g1 = Group.create! name: "Knitting",\
+                   description: "yo",\
+                   neighborhood_id: 1,\
+                   kind: 0,\
+                   state: 1
 
 RSpec.describe Post, type: :model do
   it "is valid" do
-    p1 = Post.new(user_id: u1.id, content: post_content)
+    p1 = Post.new(user_id: u1.id, content: post_content, group_id: g1.id)
     expect(p1.save).to eq true
+    expect(Post.first).to eq p1
     expect(p1.content).to eq post_content
   end
   it "is invalid without an id" do
-    p1 = Post.new(content: post_content)
-    expect(p1.save).to eq(false)
+    p1 = Post.new(content: post_content, group_id: g1.id)
+    expect(p1.save).to eq false
   end
   it "is invalid without content" do
-    p1 = Post.new(user_id: u1.id)
-    expect(p1.save).to eq(false)
+    p1 = Post.new(user_id: u1.id, group_id: g1.id)
+    expect(p1.save).to eq false
   end
   it "is invalid without a group" do
     p1 = Post.new(user_id: u1.id, content: post_content)
-    expect(p1.save).to eq true
-    expect(p1.group_id).to exist
+    expect(p1.save).to eq false
   end
   it "is ordered by most recent first"
 
@@ -35,6 +40,7 @@ RSpec.describe Post, type: :model do
   after(:all) do
     if Rails.env.test?
       u1.destroy
+      g1.destroy
     end
   end
 end
