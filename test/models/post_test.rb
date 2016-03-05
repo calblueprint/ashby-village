06874009b519369dedc9c9b0cf8b25d0ba36cd9c
piccoloman <joeypereira@sbcglobal.net>
2016-03-05 14:09:48 -0800

    annotated everything
    Summary: added the tests to the annotation
    
    Test Plan: ran bundle exec annotate
    
    Projects: #ashby
    
    Differential Revision: http://phab.calblueprint.org/D321

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :integer
+#  title      :text
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: replies
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  user_id    :integer
+#  post_id    :integer
+#  group_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ReplyTest < ActiveSupport::TestCase
