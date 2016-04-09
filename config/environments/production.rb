f06d7e288ac5ed3f26f63909e8afc8203ba3bfac
Nikita Rau <raun@berkeley.edu>
2016-04-09 16:36:39 -0700

    Reset password for heroku
    Summary: Edit test.rb to actually send emails
    
    Test Plan: na
    
    Reviewers: piccoloman
    
    Subscribers: gracesnoh, shan, helenc
    
    Projects: #ashby
    
    Differential Revision: http://phab.calblueprint.org/D440

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "ashby-prod.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "ashby-staging.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "127.0.0.1",
     port: 25,
-    domain: "ashby-prod.herokuapp.com"
+    domain: "ashby-staging.herokuapp.com"
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
