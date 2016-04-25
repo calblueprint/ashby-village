669daf8d05dfa74c1f5f9a4d7333230079265b25
Nikita Rau <raun@berkeley.edu>
2016-04-24 21:47:12 -0700

    Change email for repeated events
    Summary: Notify users that this is a repeated event and they must rsvp again
    
    Test Plan: na
    
    Reviewers: piccoloman
    
    Subscribers: helenc, shan, gracesnoh
    
    Projects: #ashby
    
    Differential Revision: http://phab.calblueprint.org/D544

--- app/mailers/ashby_mailer.rb
+++ app/mailers/ashby_mailer.rb
@@ -21,4 +21,11 @@ class AshbyMailer < ApplicationMailer
     @event = event
     mail(to: @user.email, subject: @event.title + " Is Cancelled")
   end
+
+  def email_repeat(user, group, event)
+    @user = user
+    @group = group
+    @event = event
+    mail(to: @user.email, subject: "You're Invited to " + @event.title + "!")
+  end
 end
