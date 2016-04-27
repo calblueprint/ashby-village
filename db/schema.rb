be5aa1d170547eb0698691d3245fccccf5fddde6
piccoloman <joeypereira@sbcglobal.net>
2016-04-27 16:48:38 -0700

    schema keeps going out of sync

--- db/schema.rb
+++ db/schema.rb
@@ -102,17 +102,6 @@ ActiveRecord::Schema.define(version: 20160425212622) do
   add_index "invites", ["event_id"], name: "index_invites_on_event_id", using: :btree
   add_index "invites", ["user_id"], name: "index_invites_on_user_id", using: :btree
 
-  create_table "neighborhoods", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "location"
-    t.integer  "group_id"
-  end
-
-  add_index "neighborhoods", ["group_id"], name: "index_neighborhoods_on_group_id", using: :btree
-
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
@@ -152,13 +141,6 @@ ActiveRecord::Schema.define(version: 20160425212622) do
 
   add_index "user_groups", ["group_id"], name: "index_user_groups_on_group_id", using: :btree
   add_index "user_groups", ["user_id"], name: "index_user_groups_on_user_id", using: :btree
-
-  create_table "user_neighborhoods", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.integer  "neighborhood_id"
-    t.integer  "user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                      default: "",   null: false
